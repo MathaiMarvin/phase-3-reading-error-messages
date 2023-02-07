26979487d9154c72ea4188d8d9b61c5da3fc6520
m-a-t-h-a-i <mathaimarvin574@gmail.com>
2023-02-07 12:23:21 +0300

    Completed assigment

--- lib/a_name_error.rb
+++ lib/a_name_error.rb
@@ -1,3 +1,10 @@
-"hello world"
+hello_world = "hello world"
 
 hello_world
+
+# Trying to access a variable before assigning a value results in a NameError fixed by assigning some value
+# Types of errors
+# Name errors
+#Syntax errors - result from aresult of incorrect syntax
+# Type Errors - when you try and do a mathematical operation on two objects of a differnt type
+# Division Errors - when a number is divided by 0
